91fece5e888d0f45c1029a8dc0adc6656c08df29
ah7566235-svg <ah7566235@gmail.com>
2026-08-05 14:20:22 +0300

    Add ADC configuration header file

---
+++ ADC_Alaa/ADC_config.h
@@ -0,0 +1,14 @@
+#ifndef ADC_CONFIG_H_
+#define ADC_CONFIG_H_
+
+#define ADC_VREF                    ADC_VREF_AVCC
+
+#define ADC_PRESCALER               ADC_PRESCALER_64
+
+#define LM35_ADC_CHANNEL            ADC_CHANNEL0
+
+#define LM35_SAMPLE_COUNT           8u
+
+#define LM35_HUMAN_TEMP_THRESHOLD   30.0f
+
+#endif 
