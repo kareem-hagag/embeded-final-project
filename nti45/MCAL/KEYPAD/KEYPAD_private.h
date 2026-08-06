370446b3b9add8e8303f6c0c2dd241f384ab5d51
kareem-hagag <engkareemhagag@gmail.com>
2026-08-06 16:45:56 +0300

    Delete nti45 directory

--- nti45/MCAL/KEYPAD/KEYPAD_private.h
+++
@@ -1,6 +0,0 @@
-#ifndef KEYPAD_PRIVATE_H
-#define KEYPAD_PRIVATE_H
-
-#define NOT_PRESSED  0xFF
-
-#endif
--- nti45/MCAL/LCD_1/LCD_private.h
+++
@@ -1,6 +0,0 @@
-#ifndef LCD_PRIVATE_H
-#define LCD_PRIVATE_H
-
-static void LCD_voidPutonBus(u8 u8Char);
-
-#endif
